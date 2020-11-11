2bbc437c47c89cf766097f7cceba6f54253e955c
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-11-11 13:55:35 +0600

    Delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import config from './Config/config';
 import SceneMainMenu from './Scenes/SceneMainMenu';
 import SceneMain from './Scenes/SceneMain';
 import SceneGameOver from './Scenes/SceneGameOver';
-// import Entities from './Objects/Entities';
-// import GameScene from './Scenes/GameScene';
-// import BootScene from './Scenes/BootScene';
-// import PreloaderScene from './Scenes/PreloaderScene';
-// import TitleScene from './Scenes/TitleScene';
-// import OptionsScene from './Scenes/OptionsScene';
-// import CreditsScene from './Scenes/CreditsScene';
 
 class Game extends Phaser.Game {
   constructor() {
@@ -18,13 +11,6 @@ class Game extends Phaser.Game {
     this.scene.start('MainScene');
     this.scene.add('MainMenu', SceneMainMenu);
     this.scene.add('GameOver', SceneGameOver);
-    // this.scene.add('Boot', BootScene);
-    // this.scene.add('Preloader', PreloaderScene);
-    // this.scene.add('Title', TitleScene);
-    // this.scene.add('Options', OptionsScene);
-    // this.scene.add('Credits', CreditsScene);
-    // this.scene.add('Game', GameScene);
-    // this.scene.start('Game');
   }
 }
 
