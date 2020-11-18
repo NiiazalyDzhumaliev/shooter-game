ebddb093ed23a96cd845c0847cfa5349fffa70c5
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-11-18 20:10:37 +0600

    Add dots

--- src/Scenes/MainMenuScene.js
+++ src/Scenes/MainMenuScene.js
@@ -6,16 +6,16 @@ export default class SceneMainMenu extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('sprBtnPlay', 'assets/sprBtnPlay.png');
-    this.load.image('sprBtnPlayHover', 'assets/sprBtnPlayHover.png');
-    this.load.image('sprBtnPlayDown', 'assets/sprBtnPlayDown.png');
-    this.load.image('sprBtnRestart', 'assets/sprBtnRestart.png');
-    this.load.image('sprBtnRestartHover', 'assets/sprBtnRestartHover.png');
-    this.load.image('sprBtnRestartDown', 'assets/sprBtnRestartDown.png');
+    this.load.image('sky', './assets/sky.png');
+    this.load.image('sprBtnPlay', './assets/sprBtnPlay.png');
+    this.load.image('sprBtnPlayHover', './assets/sprBtnPlayHover.png');
+    this.load.image('sprBtnPlayDown', './assets/sprBtnPlayDown.png');
+    this.load.image('sprBtnRestart', './assets/sprBtnRestart.png');
+    this.load.image('sprBtnRestartHover', './assets/sprBtnRestartHover.png');
+    this.load.image('sprBtnRestartDown', './assets/sprBtnRestartDown.png');
 
-    this.load.audio('sndBtnOver', 'assets/sndBtnOver.wav');
-    this.load.audio('sndBtnDown', 'assets/sndBtnDown.wav');
+    this.load.audio('sndBtnOver', './assets/sndBtnOver.wav');
+    this.load.audio('sndBtnDown', './assets/sndBtnDown.wav');
   }
 
   create() {
